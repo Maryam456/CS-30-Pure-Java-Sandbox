b99cb4ab04724ee762028bf6fd8363a2e16daae7
Maryam Ali <37376125+Maryam456@users.noreply.github.com>
2019-11-27 10:41:05 -0700

    update

--- HelloWorld/AddNum/AddNum.java
+++ HelloWorld/AddNum/AddNum.java
@@ -1,12 +1,28 @@
+import java.io.*;
+//Global Objects
+
+  AddNum obj = new AddNum();
+//global variables
+
 public class AddNum {
- public static void main (String args[]) {
-  int num1 = 1;
-  int num2 = 2;
 
-  System.out.println("\nSum of two numbers is: " + num1 +num2);
+  public int addNum (int numA, int numB) {
+    return numA + numB;
+  }
+   public float dividNum(int numA, int numB) {
+    return numA / numB;
+
+   }
+
+  public static void main(String args[])
+ throws IOException;
+   {
 
 
- }
+    int ansSum = obj.addNum(1, 2);
+    float ansDiv = obj.divideNum (4, 2);
 
-
+    System.out.println("\nSum of two numbers is: " + ans + "\n");
+    System.out.println("\nDivision of two numbers is:" \ ans \ ans "\n");
+  }
 }
